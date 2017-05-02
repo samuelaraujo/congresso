360b45aab8f1a9d6e694ab717f6c024c9939e34e
Ubirajara Jucá <biraneto.j@gmail.com>
2017-05-02 09:11:06 -0500

    página 404

---
+++ views/404.php
@@ -0,0 +1,47 @@
+
+
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>0hh Website Template | Home :: W3layouts</title>
+    <meta name="keywords" content="404 iphone web template, Android web template, Smartphone web template, free webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+    <link href="../assets/css/404.css" rel="stylesheet" type="text/css"  media="all" />
+  </head>
+  <body>
+<script src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
+<script async type='text/javascript' src='//cdn.fancybar.net/ac/fancybar.js?zoneid=1502&serve=C6ADVKE&placement=w3layouts' id='_fancybar_js'></script>
+<body>
+    <!--start-wrap--->
+    <div class="wrap">
+      <!---start-header---->
+        <div class="header">
+          <div class="logo">
+            <h1><a href="#">Ohh</a></h1>
+          </div>
+        </div>
+        <!---728x90--->
+      <!---End-header---->
+      <!--start-content------>
+      <div class="content">
+        <img src="../assets/images/pages/error-img.png" title="error" />
+        <!---728x90--->
+        <p><span><label>O</label>hh.....</span>Você solicitou a página que não existe mais.</p>
+        
+        <a href="/">Voltar ao Início</a>
+        <!---728x90--->
+        <div class="copy-right">
+          <p>&copy; 2017 I Congresso Jurídico. Todos os direitos reservados</p>
+        </div>
+        </div>
+      <!--End-Cotent------>
+    </div>
+    <!--End-wrap--->
+  </body>
+</html>
+
