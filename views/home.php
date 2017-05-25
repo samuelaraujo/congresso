b851cd888a225a8fc8d73dc17f2bfb7ea0e2c8ab
samuel <samuelbrazdearaujo@gmail.com>
2017-05-25 07:57:48 -0500

    Remoção section testemunhos
    remoçaõ da section testemunhos e add sectin participe

--- views/home.php
+++ views/home.php
@@ -2261,6 +2261,7 @@ Inscrição para estudante válida somente para graduação.</p>
 
   
 
+
   
 
 </section>
@@ -2312,7 +2313,7 @@ Inscrição para estudante válida somente para graduação.</p>
     </section>
     /Recent News -->
 
-<!-- Testimonial -->
+<!-- Testimonial 
 <section class="page-section" id="testimonial">
   <div class="row">
     <div class="col-sm-12">
@@ -2345,7 +2346,103 @@ Inscrição para estudante válida somente para graduação.</p>
     </div>
   </div>
 </section>
-<!-- /Testimonial -->
+ /Testimonial -->
+
+<!-- Partipe deste avento -->
+ <section class="page-section" id="testimonial">
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="section-title">
+        <i class="pe-7s-like"></i>
+        <h3>Participe deste evento</h3>
+        <p class="sub-title">Eventos culturais, Stands, Sorteios e muitas Promoções. Faça um investimento na sua carreira profissional</p>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-12 col-md-12">
+       <div class="blog-posts">
+                              <div class="col-xs-12 col-sm-12 col-md-6">
+                                  <article class="post clearfix maxwidth500">
+                                      <div class="col-md-12 col-lg-6 p-0">
+                                          <div class="entry-header">
+                                              <div class="post-thumb">
+                                                  <img src="/Content/images/slide/estande_anhembi.jpg" alt="Estandes" title="Estandes" class="img-responsive img-fullwidth">
+                                              </div>
+                                          </div>
+                                      </div>
+                                      <div class="col-md-12 col-lg-6 p-0">
+                                          <div class="entry-content pt-10 pr-30 pl-40">
+                                              <h3 class="title text-theme-colored line-height-auto"><a href="/Home/Estandes" title="Estandes">Estandes</a></h3>
+                                              <p>O evento contará com uma área composta por 250 estandes, com expectativa de público de mais de 20 mil pessoas. Reserve seu espaço.</p>
+                                              <a href="/Home/Estandes" class="btn btn-colored btn-theme-colored text-uppercase mb-md-40">Saiba mais</a>
+                                              <div class="clearfix"></div>
+                                          </div>
+                                      </div>
+                                  </article>
+                              </div>
+                              <div class="col-xs-12 col-sm-12 col-md-6">
+                                  <article class="post clearfix maxwidth500">
+                                      <div class="col-md-12 col-lg-6 p-0">
+                                          <div class="entry-header">
+                                              <div class="post-thumb">
+                                                  <img src="/Content/images/slide/pacotes-viagem.jpg" alt="Pacotes de viagem" title="Pacotes de viagem" class="img-responsive img-fullwidth">
+                                              </div>
+                                          </div>
+                                      </div>
+                                      <div class="col-md-12 col-lg-6 p-0">
+                                          <div class="entry-content pt-10 pr-30 pl-40">
+                                              <h3 class="title text-theme-colored line-height-auto"><a href="/Home/PacotesViagem" title="Pacotes de viagem">Pacotes de viagem</a></h3>
+                                              <p>Passagens e hospedagens com valores diferenciados para o público da Conferência Nacional da Advocacia Brasileira.</p>
+                                              <a href="/Home/PacotesViagem" class="btn btn-colored btn-theme-colored text-uppercase mb-md-40">Saiba mais</a>
+                                              <div class="clearfix"></div>
+                                          </div>
+                                      </div>
+                                  </article>
+                              </div>
+                              <div class="col-xs-12 col-sm-12 col-md-6">
+                                  <article class="post clearfix maxwidth500">
+                                      <div class="col-md-12 col-lg-6 p-0">
+                                          <div class="entry-header">
+                                              <div class="post-thumb">
+                                                  <img src="/Content/images/slide/evandro.jpg" alt="Prêmio Evandro Lins e Silva" title="Prêmio Evandro Lins e Silva" class="img-responsive img-fullwidth">
+                                              </div>
+                                          </div>
+                                      </div>
+                                      <div class="col-md-12 col-lg-6 p-0">
+                                          <div class="entry-content pt-10 pr-30 pl-40">
+                                              <h3 class="title text-theme-colored line-height-auto"><a href="premio-evandro-lins.html" title="Prêmio Evandro Lins e Silva">Prêmio Evandro Lins e Silva</a></h3>
+                                              <p>Participe da 5ª edição do Prêmio Evandro Lins e Silva.</p>
+                                              <a class="btn btn-colored btn-gray text-uppercase mb-md-40">Em breve</a>
+                                              <div class="clearfix"></div>
+                                          </div>
+                                      </div>
+                                  </article>
+                              </div>
+                              <div class="col-xs-12 col-sm-12 col-md-6">
+                                  <article class="post clearfix maxwidth500">
+                                      <div class="col-md-12 col-lg-6 p-0">
+                                          <div class="entry-header">
+                                              <div class="post-thumb">
+                                                  <img src="/Content/images/slide/tribuna-livre.jpg" alt="Tribuna Livre" title="Tribuna Livre" class="img-responsive img-fullwidth">
+                                              </div>
+                                          </div>
+                                      </div>
+                                      <div class="col-md-12 col-lg-6 p-0">
+                                          <div class="entry-content pt-10 pr-30 pl-40">
+                                              <h3 class="title text-theme-colored line-height-auto"><a href="/Home/TribunaLivre" title="Tribuna Livre">Tribuna Livre</a></h3>
+                                              <p>A Tribuna Livre contará com uma sala exclusiva e se desenvolverá em 05 sessões, paralelas aos painéis da Conferência. Envie seu trabalho.</p>
+                                              <a href="/Home/TribunaLivre" class="btn btn-colored btn-theme-colored text-uppercase mb-md-40">Saiba mais</a>
+                                              <div class="clearfix"></div>
+                                          </div>
+                                      </div>
+                                  </article>
+                              </div>
+        </div>
+    </div>
+  </div>
+</section>
+<!-- /Partipe deste avento -->
 
 <!-- FAQ -->
 <section class="page-section" id="faq">
@@ -2373,20 +2470,7 @@ Inscrição para estudante válida somente para graduação.</p>
             </div>
           </div>
         </div>
-        <div class="panel panel-default">
-          <div class="panel-heading" role="tab" id="headingTwo">
-            <h4 class="panel-title">
-              <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-              Quais são os requisitos técnicos para assistir ao Congresso?
-              </a>
-            </h4>
-          </div>
-          <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-            <div class="panel-body">
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-            </div>
-          </div>
-        </div>
+        
         <div class="panel panel-default">
           <div class="panel-heading" role="tab" id="headingThree">
             <h4 class="panel-title">
@@ -2401,6 +2485,22 @@ Inscrição para estudante válida somente para graduação.</p>
             </div>
           </div>
         </div>
+        
+        <div class="panel panel-default">
+          <div class="panel-heading" role="tab" id="headingTwo">
+            <h4 class="panel-title">
+              <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              Quais são os requisitos técnicos para assistir ao Congresso?
+              </a>
+            </h4>
+          </div>
+          <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+            <div class="panel-body">
+              Não há pre-requisitos, qualquer profissional pode participar do evento na categoria outros profissionais.
+            </div>
+          </div>
+        </div>
+
         <div class="panel panel-default">
           <div class="panel-heading" role="tab" id="headingFour">
             <h4 class="panel-title">
@@ -2429,20 +2529,7 @@ Inscrição para estudante válida somente para graduação.</p>
             </div>
           </div>
         </div>
-        <div class="panel panel-default">
-          <div class="panel-heading" role="tab" id="headingSix">
-            <h4 class="panel-title">
-              <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
-              Pellentesque iaculis facilisis eros id molestie?
-              </a>
-            </h4>
-          </div>
-          <div id="collapseSix" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSix">
-            <div class="panel-body">
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-            </div>
-          </div>
-        </div>
+    
       </div>
     </div>
   </div>
